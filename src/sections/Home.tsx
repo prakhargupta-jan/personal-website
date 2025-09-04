fd2521d1734db8469fad7bad3ef5c11105c389f5
prakhargupta-jan <prakhargupta.codes@gmail.com>
2025-09-04 10:46:57 +0530

    trying mounting home after loading for proper animations

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -1,11 +1,14 @@
 import {motion} from 'framer-motion'
 import { Link } from "react-router-dom"
+import { useContext } from 'react'
+import { FetcherContext } from '@/components/Fetcher'
 
 const Home = () => {
+    const {state } = useContext(FetcherContext);
+    if (state.loading) return <div>is this a hidden message ??</div>;
     return (
         <div className="absolute top-1/2 transform -translate-y-1/2  text-white">
             <span className="text-4xl font-[Rubik] text-green-400" >Hello I'm</span>
-            {/* TypeWriter Effect */}
             <motion.div initial={{ opacity: 0, x: -20 }} animate={{ opacity: 1, x: 0 }} className="text-7xl font-bold my-2">Prakhar Gupta</motion.div>
             <motion.div initial={{ opacity: 0, x: -20 }} animate={{ opacity: 1, x: 0, transition: { delay: 0.2 } }}  className="text-2xl mt-4">A passionate Software Engineer</motion.div>
             <motion.div initial={{ opacity: 0, x: -20 }} animate={{ opacity: 1, x: 0, transition: { delay: 0.4 } }}  className="text-xl mt-2">Specializing in building exceptional Backend Systems.</motion.div>
